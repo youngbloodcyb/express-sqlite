8d341e0325acb27620baacf945fd5183efe8c223
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-06 20:29:42 -0700

    create express app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const { PORT } = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
