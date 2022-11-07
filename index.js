da66121cf8ee838e0779b583b84ef12ad651e155
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-06 21:13:18 -0700

    create database sqlite

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 app.set("view engine", "ejs");
@@ -8,6 +9,28 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 const { PORT } = process.env;
+
+const db_name = path.join(__dirname, "data", "apptest.db");
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'apptest.db'");
+});
+
+const sql_create = `CREATE TABLE IF NOT EXISTS Books (
+    Book_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Title VARCHAR(100) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
+    Comments TEXT
+);`;
+  
+db.run(sql_create, err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Successful creation of the 'Books' table");
+});
 
 app.get("/", (req, res) => {
     res.render("index");
