75c6a15d6c3ecfd9aba5b867ebf202523a651ffa
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-06 20:39:41 -0700

    add dotenv package

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-});
-
 app.get("/", (req, res) => {
     res.send("Hello world");
+});
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log(`Server running on ${PORT}`);
 });
